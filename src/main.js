a4e3d146f2e56c4cf228814b12c192873f7f7f3b
yuzhenhao <yuzhenhao@bsgchina.com>
2022-03-18 19:39:34 +0800

    添加 element-ui组件和样式

--- src/main.js
+++ src/main.js
@@ -8,6 +8,20 @@ Vue.config.productionTip = false;
 // 加载 svg icons
 import "./icons";
 
+// 加载 Element UI组件
+
+import Cookies from "js-cookie";
+import Element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+// 加载全局样式
+import "./styles/index.scss";
+
+Vue.use(Element, {
+  // set element-ui default size
+  size: Cookies.get("size") || "mini",
+});
+
 new Vue({
   router,
   store,
